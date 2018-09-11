d51a48dd73c923da7e68c4e719a0da64be32ad2a
amrshurman <amrshurman@cmail.carleton.ca>
2018-09-11 19:23:28 -0400

    added a lot of bj functionality

---
+++ src/main/java/comp3004/blackJack.java
@@ -0,0 +1,83 @@
+package comp3004;
+
+import java.util.*;
+
+public class blackJack extends card{
+
+	public static void main(String[] args) {
+		boolean breaked=false;
+		boolean soft17=false;
+		int result=0;
+		int resultD=0;
+	String input;
+	 Scanner sc = new Scanner(System.in);
+	
+	 card card1 = new card();
+	 card card2 = new card();
+	 card cardD1 = new card();
+	 card cardD2 = new card();
+	 resultD=calculate(cardD1,resultD);
+	 resultD=calculate(cardD2,resultD);
+	System.out.println("Dealer has drawn a " + cardD1.getCard()+ ", the other card he drew is hidden for now.");
+	System.out.println(card1.getCard());
+	System.out.println(card2.getCard());
+	result=calculate(card1,result);
+	result=calculate(card2,result);
+	System.out.println("Total : " + result);
+	while (true) {
+		if (result==21) {
+			System.out.println("You got BlackJack!");
+		}
+		else if ((result<21)&&(resultD<21)){
+	System.out.println("Hit(H) or Stand(S)?");
+	input = sc.nextLine();
+	if (input.equals("H")) {
+		card card3 = new card();
+	System.out.println(card3.getCard());
+	result= (calculate(card3,result));
+	System.out.println("Total : " + result);
+	}
+	else {
+		System.out.println("You stand");
+		break;
+	}
+	}
+	else {
+		System.out.println("You break");
+		breaked=true;
+		break;
+	}
+	}
+	while (true) {
+		if (breaked==true) {
+			System.out.println("Dealer wins because you break.");
+			break;
+		}
+		else if (resultD==21){
+		System.out.println("Dealer wins with BlackJack");
+		break;
+	}
+	else if ((resultD>result)&&(resultD<22)) {
+		System.out.println("Dealer wins with " + resultD);
+		break;
+	}
+	else if ((resultD<17)||(soft17==true)) {
+		card cardD3 = new card();
+		System.out.println(cardD3.getCard());
+		resultD= (calculate(cardD3,resultD));
+		System.out.println("Dealer has a Total : " + resultD);
+	}
+	else if (resultD>21) {
+		System.out.println("Dealer breaks, you win");
+		break;
+	}
+	else {
+		System.out.println("Dealer stands");
+		if (result>resultD) {
+			System.out.println("You win by having a closer number to 21.");
+		break;
+		}
+	}
+	}
+	}
+}
---
+++ src/main/java/comp3004/card.java
@@ -0,0 +1,119 @@
+package comp3004;
+
+import java.util.Random;
+
+public class card {
+	
+  //got some inspiration from https://codereview.stackexchange.com/questions/125873/implementation-of-a-card-class-in-java 
+	 private String Suit, Rank, Color, Card;
+	 private static final Random generator = new Random();
+	 
+	    private static String[] Suits = {"Hearts", "Spades", "Diamonds", "Clubs"};
+	    private static String[] Ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8",
+	    "9", "10", "Jack", "Queen", "King"};
+
+	    int randomSuits = generator.nextInt(Suits.length);
+	    int randomRanks = generator.nextInt(Ranks.length);
+
+	    card(){
+	    	 Suit = Suits[randomSuits];
+	         Rank = Ranks[randomRanks];
+	         Card = Suit.charAt(0) + "" + Rank.charAt(0);
+	         // Setting the color of the card in dependence of the suit
+	         if (Suit.equals("Hearts") || Suit.equals("Diamonds")) {
+	             Color = "Red";
+	         }
+	         else if (Suit.equals("Spades") || Suit.equals("Clubs")){
+	             Color = "Black";
+	         }
+	    }
+	
+	    public String getCard() {
+	         return Card;
+	     }
+	    
+	    public String getSuit() {
+	         return Suit;
+	     }
+
+	     public String getRank() {
+	         return Rank;
+	     }
+
+	     public String getColor() {
+	         return Color;
+	     }
+	     
+	public static int validateCard(String s) {
+		if (s.charAt(0)=='H') {
+			System.out.println("HEARTS card accquired");
+		}
+			else if (s.charAt(0)=='S') {
+				System.out.println("SPADES card accquired");
+			}
+			else if (s.charAt(0)=='D') {
+				System.out.println("DIAMONDS card accquired");
+			}
+			else if (s.charAt(0)=='C') {
+				System.out.println("CLUBS card accquired");
+			}
+		
+		else { 
+			System.out.println("Invalid Suit selected");
+			System.out.println(s.charAt(0));
+			return 0;
+		}
+		
+			if (s.charAt(1)=='A') {
+				return 1;
+			}
+			else if	(s.charAt(1)=='2') {
+					return 2;
+				}
+			else if	(s.charAt(1)=='3') {
+				return 3;
+			}
+			else if	(s.charAt(1)=='4') {
+				return 4;
+			}
+			else if	(s.charAt(1)=='5') {
+				return 5;
+			}
+			else if	(s.charAt(1)=='6') {
+				return 6;
+			}
+			else if	(s.charAt(1)=='7') {
+				return 7;
+			}
+			else if	(s.charAt(1)=='8') {
+				return 8;
+			}
+			else if	(s.charAt(1)=='9') {
+				return 9;
+			}
+			else if	((s.charAt(1)=='1')&&(s.charAt(2)=='0')) {
+				return 10;
+			}
+			else if ((s.charAt(0)!='K') || (s.charAt(0)!='Q') || (s.charAt(0)!='J'))
+				return 10;
+			
+			else {
+			System.out.println("Invalid Rank selected");
+			return 0;
+			}
+		}
+	
+	
+	public static int calculate(card card, int result) {
+		int value=validateCard(card.getCard());
+		
+		if ((value==1) && (value+result<11)) {
+			result=result+10;
+		}
+		result=result+value;
+		return result;
+	}
+	
+	}
+	
+
